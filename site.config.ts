d387970368ff023f7925d10a6b39126d254ccca3
Natan <32833012+baronnath@users.noreply.github.com>
2024-11-08 16:04:09 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '12ff70952caa80b59edec6b3595943f4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '5c29a4c2-65f2-428d-9552-7a0c7e22a1f7',
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Thēria World',
+  domain: 'theriarpg.com',
+  author: 'nathanlosabe',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Thēria RPG Game',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // instagram: 'theria.rpg',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -28,7 +29,7 @@ export default siteConfig({
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
+  defaultPageCoverPosition: 0.2,
 
   // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: true,
@@ -42,24 +43,41 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/home': '12ff70952caa80b59edec6b3595943f4',
+    '/reglas': 'fdba6295c2444e7dabdc6e10d925e3f1',
+    '/camapanas': 'f1520856273f4f6b9004ceeea144c69c'
+  },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Rules Set',
+      pageId: 'fdba6295c2444e7dabdc6e10d925e3f1'
+    },      
+    {
+      title: 'Campaigns',
+      pageId: 'f1520856273f4f6b9004ceeea144c69c'
+    },
+    {
+      title: 'Characters',
+      pageId: 'd3646eadfa394a568e65b116146c80a8'
+    },
+    {
+      title: 'Monsters',
+      pageId: 'fc7ed021048949db868448a44ccf26d8'
+    },
+    {
+      title: 'Abilities',
+      pageId: '8e7bf1f12333432089c9ca6b7435a2d5'
+    },
+    {
+      title: 'Items',
+      pageId: '836b85b669734c2b9dca62515eea7f68'
+    }
+  ]
 })
